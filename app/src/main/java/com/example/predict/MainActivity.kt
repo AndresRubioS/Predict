18587880593343fceabee1d71d9c7d28074983e9
AndresRubioS <andres-rubio-salas@hotmail.com>
2023-05-08 04:11:12 +0200

    navigation register

--- app/src/main/java/com/example/predict/MainActivity.kt
+++ app/src/main/java/com/example/predict/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.predict
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.predict.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        pantallaRegistro()
+
     }
+
+    private fun pantallaRegistro() {
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, register_activity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
