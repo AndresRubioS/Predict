243f00e0812caace94f7496cefc8fdbdeca16b49
AndresRubioS <andres-rubio-salas@hotmail.com>
2023-05-09 18:40:15 +0200

    no message

--- app/src/main/java/com/example/predict/ui/NavBar/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/predict/ui/NavBar/BottomNavigationActivity.kt
@@ -1,14 +1,10 @@
 package com.example.predict.ui.NavBar
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.Profile
-import android.provider.ContactsContract.Settings
 import androidx.fragment.app.Fragment
 import com.example.predict.R
 import com.example.predict.databinding.ActivityBottomNavigationBinding
-import com.example.predict.databinding.FragmentHomeBinding
 
 class BottomNavigationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBottomNavigationBinding
@@ -21,8 +17,10 @@ class BottomNavigationActivity : AppCompatActivity() {
 
             when(it.itemId) {
                 R.id.home -> replaceFragment(HomeFragment())
-                R.id.profile -> replaceFragment(ProfileFragment())
-                R.id.settings -> replaceFragment(SettingFragment())
+                R.id.noticias -> replaceFragment(NoticiasFragment())
+                R.id.predict -> replaceFragment(PredictFragment())
+                R.id.promos -> replaceFragment(PromosFragment())
+                R.id.ranking -> replaceFragment(RankingFragment())
 
                 else -> {
 
--- app/src/main/java/com/example/predict/ui/NavBar/PredictFragment.kt
+++ app/src/main/java/com/example/predict/ui/NavBar/PredictFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
+ * Use the [PredictFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingFragment : Fragment() {
+class PredictFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class SettingFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
+            PredictFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
